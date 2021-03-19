322d7755efb84aac5ef4195d669514b62f965570
sercan <sercanyanbuloglu@gmail.com>
2021-03-19 17:57:23 +0300

    Initial commit

---
+++ src/scripts/index.js
@@ -0,0 +1,21 @@
+import '../styles/style.css';
+
+
+
+import  'popper.js';
+import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+
+
+
+//images
+
+
+
+
+if (process.env.NODE_ENV === 'production') {
+  require('../index.html');
+}
+
